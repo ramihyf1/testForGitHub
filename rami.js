d62d2da595ee7ea50d83dd49a6d0fb8d26880ad7
Your Name <you@example.com>
2019-03-26 22:10:15 +0100

    again

---
+++ rami.js
@@ -0,0 +1,14 @@
+/* setTimeout(function(){
+    document.getElementById("id").innerHTML = "hello id"
+},3000);
+
+setTimeout(function(){ document.getElementById("idd").innerHTML = "hello idd"},3000);
+
+console.log("test");
+
+setTimeout(function() {
+    document.getElementById("iddd").innerHTML = "hello iddd"
+},3000); */
+document.addEventListener('mousemove',function() {
+        console.log('hello mouse');
+})
